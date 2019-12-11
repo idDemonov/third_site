d3230d5c88006a058dbd597ace2889472b638615
demonov <rustamdemonov@gmail.com>
2019-12-11 16:33:34 +0300

    доделал checkbox-list

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,14 @@
-checkbox ();
+checkboxList();
 
-function checkbox () {
-	let checkboxList = document.querySelector(".checkbox-list__icon");
-	let checkboxButton = document.querySelector(".checkbox-list__button");
-	checkboxButton.onclick = () => {
-		checkboxList.classList.add("checkbox-list__icon--activ");
+function checkboxList () {
+	let checkClick = document.querySelectorAll(".checkbox-list__button"); // Клик
+	let checkIcon = document.querySelectorAll(".checkbox-list__icon"); // Повесить поворот иконки
+	let checkDrop = document.querySelectorAll(".checkbox-dropdown"); // Повесить раскрытие
+
+	for (let i = 0; i < checkClick.length; i++) {
+		checkClick[i].onclick = () => {
+			checkDrop[i].classList.toggle('checkbox-dropdown--expanded');
+			checkIcon[i].classList.toggle('checkbox-list__icon--activ');
+		}
 	}
 }
