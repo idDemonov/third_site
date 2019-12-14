14a281e0e0b42752a116abce7e12487d3a5d55a4
demonov <rustamdemonov@gmail.com>
2019-12-14 22:29:02 +0300

    Сделал лайк

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
-checkboxList();
+showCheckboxList();
+creatLike ();
 
-function checkboxList () {
+
+
+
+
+
+function showCheckboxList () {
 	let checkClick = document.querySelectorAll(".checkbox-list__button"); // Клик
 	let checkIcon = document.querySelectorAll(".checkbox-list__icon"); // Повесить поворот иконки
 	let checkDrop = document.querySelectorAll(".checkbox-dropdown"); // Повесить раскрытие
@@ -9,6 +15,25 @@ function checkboxList () {
 		checkClick[i].onclick = () => {
 			checkDrop[i].classList.toggle('checkbox-dropdown--expanded');
 			checkIcon[i].classList.toggle('checkbox-list__icon--activ');
+		}
+	}
+}
+
+function creatLike () { // Функция заглушка, сделал просто для наглядности изменения лайка, переделать!!!
+	let likeClick = document.querySelectorAll(".like__box"); // Клик
+	let likeNum = document.querySelectorAll(".like__text"); // Повесить изменение значения
+
+	for (let i = 0; i < likeClick.length; i++) {
+		likeClick[i].onclick = () => {
+			let valueLike = likeNum[i].innerHTML;
+			valueLike = +valueLike;
+			if (valueLike == 11 || valueLike == 2) {
+				valueLike = valueLike + 1;
+				likeNum[i].innerHTML = (valueLike);
+			} else if (valueLike == 12 || valueLike == 3) {
+				valueLike = valueLike - 1;
+				likeNum[i].innerHTML = (valueLike);
+			}
 		}
 	}
 }
