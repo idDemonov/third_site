0ba95e23ee10080b2259e1523b58c9245b579b41
demonov <rustamdemonov@gmail.com>
2020-02-20 23:44:43 +0300

    Правил like безуспешно

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,31 @@
 'use strict'
-import {showCheckboxList} from './showCheckboxList.js';
-
-
 showCheckboxList();
-creatLike ();
 
 
+function showCheckboxList () {
+	let checkClick = document.querySelectorAll(".checkbox-list__button"); // Клик
+	let checkIcon = document.querySelectorAll(".checkbox-list__icon"); // Повесить поворот иконки
+	let checkDrop = document.querySelectorAll(".checkbox-dropdown"); // Повесить раскрытие
 
+	for (let i = 0; i < checkClick.length; i++) {
+		checkClick[i].onclick = () => {
+			checkDrop[i].classList.toggle('checkbox-dropdown--expanded');
+			checkIcon[i].classList.toggle('checkbox-list__icon--activ');
+		}
+	}
+}
 
+let likes = document.querySelectorAll('.like__box');
+for (let box of likes) {
+	box.onclick = countLike;
+}
 
-
-
-
-
-function creatLike () { // Функция заглушка, сделал просто для наглядности изменения лайка, переделать!!!
-	let likeClick = document.querySelectorAll(".like__box"); // Клик
-	let likeNum = document.querySelectorAll(".like__text"); // Повесить изменение значения
-
-	for (let i = 0; i < likeClick.length; i++) {
-		likeClick[i].onclick = () => {
-			let valueLike = likeNum[i].innerHTML;
-			valueLike = +valueLike;
-			if (valueLike == 11 || valueLike == 2) {
-				valueLike = valueLike + 1;
-				likeNum[i].innerHTML = (valueLike);
-			} else if (valueLike == 12 || valueLike == 3) {
-				valueLike = valueLike - 1;
-				likeNum[i].innerHTML = (valueLike);
-			}
+function countLike() {
+	for(let like of likes) {
+		if(like.previousElementSibling.checked == true) {
+			like.nextElementSibling.innerText = ++like.nextElementSibling.innerText;
+		} else {
+			like.nextElementSibling.innerText = --like.nextElementSibling.innerText;
 		}
 	}
 }
