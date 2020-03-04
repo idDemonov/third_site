8d9de35e7dd01cbfd2e7351451bc89d6dbe3c8d9
demonov <rustamdemonov@gmail.com>
2020-03-04 22:14:07 +0300

    Правки багов

--- src/script/app.js
+++ src/script/app.js
@@ -33,38 +33,64 @@ function liked() {
 
 
 
-// События связанные с выпадающим списком комнат
-const calcRoomsDrop = document.querySelectorAll('.rooms-dropdown');
-for(let block of calcRoomsDrop) {
-	
-	block.onclick = (event) => {
-		let target = event.target; 
-		if (target.tagName != 'BUTTON' && target.tagName != 'INPUT') return; 
 
-		if(target.className == 'cart-control__button cart-control__button--reduce') {
-			target.nextElementSibling.value = (target.nextElementSibling.value > target.nextElementSibling.min) ? 
-			--target.nextElementSibling.value : target.nextElementSibling.value; 
-			let input = block.getElementsByTagName('input').local;
-			let lowdown = block.querySelectorAll(".cart-control__quantity");
-			calcRoomsWriteInput(input, lowdown);
-		}
-		if(target.className == 'cart-control__button cart-control__button--add') {
-			target.previousElementSibling.value = (target.previousElementSibling.value < target.previousElementSibling.max) ? 
-			++target.previousElementSibling.value : target.previousElementSibling.value; 
-			let input = block.getElementsByTagName('input').local;
-			let lowdown = block.querySelectorAll(".cart-control__quantity");
-			calcRoomsWriteInput(input, lowdown);
-		}
-		if(target.className == 'rooms-dropdown__input') {
-			target.classList.add('rooms-dropdown__input--expanded');
-			target.parentElement.nextElementSibling.classList.add('rooms-dropdown__box--expended');
-		}
-		if(target.className == 'rooms-dropdown__button') {
-			target.previousElementSibling.classList.toggle('rooms-dropdown__input--expanded');
-			target.parentElement.nextElementSibling.classList.toggle('rooms-dropdown__box--expended');
+const calcRoomsDrop = document.querySelectorAll('.rooms-dropdown');
+setEventDropBox(calcRoomsDrop, 'room');
+
+const calcGuestsDrop = document.querySelectorAll('.guests-dropdown');
+setEventDropBox(calcGuestsDrop, 'guest');
+
+// Функция навешивает события связанные с выпадающим списком комнат
+function setEventDropBox(elements, type) {
+	for(let block of elements) {
+	
+		block.onclick = (event) => {
+			let target = event.target;
+			 
+			if (target.tagName != 'BUTTON' && target.tagName != 'INPUT') return; 
+
+			if(type == 'guest') {
+				if(target.className == 'guests-dropdown__input') {
+					target.classList.add('guests-dropdown__input--expanded');
+					target.parentElement.nextElementSibling.classList.add('guests-dropdown__box--expended');
+				}
+				else if(target.className == 'guests-dropdown__button') {
+					target.previousElementSibling.classList.toggle('guests-dropdown__input--expanded');
+					target.parentElement.nextElementSibling.classList.toggle('guests-dropdown__box--expended');
+				}
+			}
+			else if(type == 'room') {
+				if(target.className == 'rooms-dropdown__input') {
+					target.classList.add('rooms-dropdown__input--expanded');
+					target.parentElement.nextElementSibling.classList.add('rooms-dropdown__box--expended');
+				}
+				else if(target.className == 'rooms-dropdown__button') {
+					target.previousElementSibling.classList.toggle('rooms-dropdown__input--expanded');
+					target.parentElement.nextElementSibling.classList.toggle('rooms-dropdown__box--expended');
+				}
+			}
+	
+			if(target.className == 'cart-control__button cart-control__button--reduce') {
+				target.nextElementSibling.value = (target.nextElementSibling.value > target.nextElementSibling.min) ? 
+				--target.nextElementSibling.value : target.nextElementSibling.value; 
+				let input = block.getElementsByTagName('input').local;
+				let lowdown = block.querySelectorAll(".cart-control__quantity");
+				if(type == 'guest') calcGuestsWriteInput(input, lowdown);
+				else if(type == 'room') calcRoomsWriteInput(input, lowdown);
+				
+			}
+			if(target.className == 'cart-control__button cart-control__button--add') {
+				target.previousElementSibling.value = (target.previousElementSibling.value < target.previousElementSibling.max) ? 
+				++target.previousElementSibling.value : target.previousElementSibling.value; 
+				let input = block.getElementsByTagName('input').local;
+				let lowdown = block.querySelectorAll(".cart-control__quantity");
+				if(type == 'guest') calcGuestsWriteInput(input, lowdown);
+				else if(type == 'room') calcRoomsWriteInput(input, lowdown);
+			}
 		}
 	}
 }
+
 // Фукнция подсчитывает комнаты и записывает в инпут
 function calcRoomsWriteInput(input, lowdown) {
 	let rooms = {
@@ -81,4 +107,20 @@ function calcRoomsWriteInput(input, lowdown) {
 	
 	rooms.bathroom = (rooms.bathroom == 0) ? 'без' : rooms.bathroom;
 	input.value = `${rooms.bedroom} ${word1}, ${rooms.bed} ${word2}, ${rooms.bathroom} ${word3}`
+}
+
+// Фукнция подсчитывает людей и записывает в инпут
+function calcGuestsWriteInput(input, lowdown) {
+	let people = {
+		adult: 0,
+		children: 0,
+		babies: 0,
+	}
+	for (let type of lowdown) {
+		people[type.dataset.typeRoom] = type.value;
+	}
+	let count = Object.values(people).reduce((a, b) => +a + +b, 0);
+	let word = (count == 1) ? 'гость' : (count < 5) ? 'гостя' : 'гостей';
+	input.value = `${count} ${word}`
+	if(count == 0) input.value = 'Сколько гостей'
 }
