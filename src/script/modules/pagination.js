865204f9367e536d097d6dea2c23b999b09ecd34
demonov <rustamdemonov@gmail.com>
2020-03-18 01:55:23 +0300

    Изменение количества страниц

--- src/script/modules/pagination.js
+++ src/script/modules/pagination.js
@@ -12,26 +12,23 @@ let pointL = document.querySelector('button[data-point="left"]');
 pointL.style.display = 'none';
 document.querySelector(".pagination")
 	.addEventListener("click", event => {
-		// if (event.target !== 'BUTTON') return;
 
 		if (event.target.dataset.action == "next") nextPage();
 		if (event.target.dataset.action == "previous") previousPage();
 		if (event.target.dataset.action == "start") {
 			if(first.classList.contains('pagination__button--active')) return;
-			else {
-				
-			}
 		}
-		// else if (event.target.dataset.action == "end") end()
-
+		
 
 });
 
 const nextPage = () => {
+	
 	if(active.classList.contains('pagination__button--active')) {
 		previous.style.display = 'block';
 		active.classList.remove('pagination__button--active')
 		main.classList.add('pagination__button--active')
+		correctQuantity(+main.innerHTML);		
 	}
 	else if (after.innerHTML <= end.innerHTML - 1) {
 		after.innerHTML = ++after.innerHTML
@@ -39,22 +36,27 @@ const nextPage = () => {
 		if(main.innerHTML > 3) pointL.style.display = 'block';
 		if(after.innerHTML == 14) pointR.style.display = 'none';
 		if(after.innerHTML == 15) end.style.display = 'none';
-
+		correctQuantity(+main.innerHTML);
 	}
 	else if (+main.innerHTML == 14) {
 		next.style.display = 'none';
 		main.classList.remove('pagination__button--active')
 		after.classList.add('pagination__button--active')
-
+		console.log(+main.innerHTML + 'Finisf')
+		correctQuantity(15);
 	}
+	
+	// correctQuantity();
 }
 
 
 const previousPage = () => {
+	
 	if(after.classList.contains('pagination__button--active')) {
 		next.style.display = 'block';
 		after.classList.remove('pagination__button--active')
 		main.classList.add('pagination__button--active')
+		correctQuantity(+main.innerHTML);
 	}
 	else if (main.innerHTML > +first.innerHTML + 1) {
 		after.innerHTML = --after.innerHTML
@@ -62,13 +64,21 @@ const previousPage = () => {
 		if(main.innerHTML < 3) pointL.style.display = 'none';
 		if(after.innerHTML == 13) pointR.style.display = 'block';
 		if(after.innerHTML == 14) end.style.display = 'block';
-
+		correctQuantity(+main.innerHTML);
 	}
 	else if (+main.innerHTML == 2) {
 		previous.style.display = 'none';
 		main.classList.remove('pagination__button--active')
 		first.classList.add('pagination__button--active')
-
+		correctQuantity(1);
 	}
+	// correctQuantity();
 }
 
+const correctQuantity = (pageCount) => {
+	console.log(pageCount)
+	const value = pageCount * 12;
+	quantity.innerText = (value >= 100) ? 
+	`${value+1 - 12} – ${value} из 180 вариантов аренды`
+	: `${value+1 - 12} – ${value} из 100+ вариантов аренды`
+}
