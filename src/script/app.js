db14bd95afaddf0abca3564a065eb4a50032ff6b
demonov <rustamdemonov@gmail.com>
2020-03-21 01:05:49 +0300

    переключение при нажатии, а также стрелки

--- src/script/app.js
+++ src/script/app.js
@@ -17,9 +17,6 @@ import './modules/pagination'
 // Функции навешивает события связанные с выпадающим списком комнат и гостей
 import './modules/setEventDropBox/setEventDropBox'
 
-
-
-
 // Скрытие элемента при нажатии вне области
 // let hamburger = document.querySelector('.rooms-dropdown__form');
 // let menu = document.querySelector('.rooms-dropdown__box');
@@ -39,7 +36,7 @@ import './modules/setEventDropBox/setEventDropBox'
 //   let its_menu = target == menu || menu.contains(target);
 //   let its_hamburger = target == hamburger;
 //   let menu_is_active = menu.classList.contains('rooms-dropdown__box--expended');
-  
+
 //   if (!its_menu && !its_hamburger && menu_is_active) {
 //     toggleMenu();
 //   }
--- src/script/modules/pagination.js
+++ src/script/modules/pagination.js
@@ -1,54 +1,36 @@
-// let previous = document.querySelector(".pagination__button--previous");
-// let active = document.querySelector(".pagination__button--active");
-let first = document.querySelector(".pagination__button--first");
-// let main = document.querySelector(".pagination__button--main");
-// let after = document.querySelector(".pagination__button--after");
-// let end = document.querySelector(".pagination__button--end");
-// let next = document.querySelector(".pagination__button--next");
-// let quantity = document.querySelector(".pagination__quantity");
-// let pointR = document.querySelector('button[data-point="right"]');
-// let pointL = document.querySelector('button[data-point="left"]');
-
 class Pagination {
 	constructor(quantity) {
+		this.maxNumberPage = quantity;
+
 		this.pagination = document.querySelector('.pagination');
-		this.createPaginationNumPage(quantity, this.pagination.querySelector('.pagination__page'));
+		this.createPaginationNumPage(this.maxNumberPage, this.pagination.querySelector('.pagination__page'));
 		this.addAllEvent();
 
-		this.activPage = this.pagination.querySelector('button[data-num-page="1"]');
+		this.activPage = this.pagination.querySelector('button[data-number-page="1"]');
 		this.nextPage = this.activPage;
-		this.previousPage = this.nextPage;
-		this.endPage = this.pagination.querySelector(`button[data-num-page="${quantity}"]`);
 	}
 
-	// -->
-	nextPagef() {
-		const count = +this.nextPage.dataset.numPage + 1;
-		this.nextPage = this.pagination.querySelector(`button[data-num-page="${count}"]`) || this.pagination.querySelector(`button[data-num-page="15"]`)
 
-		// Переназначаю активную и следующую страницу
+	// <-- -->
+	togglePage(direction) {
+		let count = (direction == 'next') ? +this.nextPage.dataset.numberPage + 1 : +this.nextPage.dataset.numberPage - 1;
+
+		count = (count < 1) ? 1 : count;
+		count = (count > this.maxNumberPage) ? 15 : count;
+
+		this.nextPage = this.pagination.querySelector(`button[data-number-page="${count}"]`);
 		this.reassignmentActiv(count, this.nextPage)
-		console.log('nextPage - Следующая страница')
-	}
-
-
-	// <--
-	previousPagef() {
-		console.log('previousPage - Предыдущая страница')
 	}
 
 	// Смена активной кнопки
-	reassignmentActiv(currentPage, numPage) {
-		numPage = numPage || event.target;
-
-		if(currentPage <= +this.endPage.dataset.numPage) {
-			this.previousPage = this.activPage;
-			this.previousPage.classList.remove('pagination__button--active')
-
-			this.activPage = numPage; 
+	reassignmentActiv(currentPage, newActivPage) {
+		this.hideButtons(currentPage);
+		if(currentPage <= +this.maxNumberPage) {
+			this.activPage.classList.remove('pagination__button--active')
+			this.activPage = newActivPage; 
 			this.activPage.classList.add('pagination__button--active')
 
-			this.nextPage = this.pagination.querySelector(`button[data-num-page="${currentPage}"]`) || this.pagination.querySelector(`button[data-num-page="15"]`);
+			this.nextPage = this.pagination.querySelector(`button[data-number-page="${currentPage}"]`);
 		} 
 		
 	}
@@ -56,24 +38,37 @@ class Pagination {
 	// Навесить события
 	addAllEvent() {
 		this.pagination.addEventListener("click", event => {
-			if (event.target.dataset.action == "next") this.nextPagef();
-			else if (event.target.dataset.action == "start") this.startPagef(event.target);
-			else if (event.target.dataset.action == "previous") this.previousPagef(event.target);
-			else if (event.target.dataset.action == "end") this.endPagef(event.target);
-			else if (event.target.dataset.numPage) this.reassignmentActiv(event.target.dataset.numPage)
+			const action = event.target.dataset.action;
+			const number = event.target.dataset.numberPage;
+
+			if (action == "next") {
+				this.togglePage(action);
+			} else if (action == "previous") {
+				this.togglePage(action);
+			} else if (number) {
+				this.reassignmentActiv(number, event.target)
+			} 
 		});
 	}
 
-	// Создание узлов кнопок страниц 
+	// Создание кнопок-узлов страниц 
 	createPaginationNumPage(value, buttons) {
 		for(let i = 1; i <= value; i++) {
 			const button = document.createElement('button');
-			const text = document.createTextNode(`${i}`); 
+			const text = document.createTextNode(i); 
+			
 			button.appendChild(text);
-			button.dataset.numPage = i;
-			button.className = (i == 1) ? 'pagination__button pagination__button--active' : 'pagination__button';
+			button.dataset.numberPage = i;
+			button.className = (i != 1) ? 'pagination__button' : 'pagination__button pagination__button--active';
 			buttons.append(button)
 		}
+	}
+
+	// Скрытие лишних кнопок
+	hideButtons() {
+		// Всегда показывать первую и последнюю
+		// Показывать 2 страницы на перёд и две страницы назад
+		// активные страницы, а также первая и последняя должны разделяться троеточием 
 	}
 }
 
