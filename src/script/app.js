dada485d53fbf29f1b14aaeb1a948680f0a9613c
demonov <rustamdemonov@gmail.com>
2020-03-24 22:35:54 +0300

    Календарь, подключение

--- src/script/app.js
+++ src/script/app.js
@@ -1,43 +1,26 @@
 'use strict'
-import './vendors/nouislider'
+// Слайдер
+import {nouislider} from './vendors/nouislider'
 
+// Валидация форм
+import Bouncer from './vendors/bouncer'
+var validate = new Bouncer('form');
 
 // Выпадающий список checkbox
-import './modules/showCheckboxList'
-
+import {showCheckboxList} from './modules/showCheckboxList'
 
 // Отменить коппирование двойным кликом
-import './modules/undoUnMouseDown'
+import {undoUnMouseDown} from './modules/undoUnMouseDown'
 
 // Посчитать лайки
-import './modules/liked'
+import {liked} from './modules/liked'
+
 // Работа с пагинацией
-import './modules/pagination'
+import {pagination} from './modules/pagination'
 
 // Функции навешивает события связанные с выпадающим списком комнат и гостей
-import './modules/setEventDropBox/setEventDropBox'
+import {setEventDropBox} from './modules/setEventDropBox/setEventDropBox'
 
-// Скрытие элемента при нажатии вне области
-// let hamburger = document.querySelector('.rooms-dropdown__form');
-// let menu = document.querySelector('.rooms-dropdown__box');
-
-// const toggleMenu = () => {
-//   menu.classList.toggle('rooms-dropdown__box--expended');
-// }
-
-// hamburger.addEventListener('click', e => {
-//   e.stopPropagation();
-
-//   toggleMenu();
-// });
-
-// document.addEventListener('click', e => {
-//   let target = e.target;
-//   let its_menu = target == menu || menu.contains(target);
-//   let its_hamburger = target == hamburger;
-//   let menu_is_active = menu.classList.contains('rooms-dropdown__box--expended');
-
-//   if (!its_menu && !its_hamburger && menu_is_active) {
-//     toggleMenu();
-//   }
-// })
+// Календарь
+// import './vendors/calendar-full'
+// var validate = new Bouncer('form');
