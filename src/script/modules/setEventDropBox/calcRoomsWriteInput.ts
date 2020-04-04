f9bdb732b8c8cce3025c2fe5bcc84aa2587cbed0
demonov <rustamdemonov@gmail.com>
2020-04-05 02:01:18 +0300

    Мелкие правки

--- src/script/modules/setEventDropBox/calcRoomsWriteInput.ts
+++ src/script/modules/setEventDropBox/calcRoomsWriteInput.ts
@@ -2,7 +2,7 @@ export const calcRoomsWriteInput = (input, lowdown) => {
   let rooms = {
     bedroom: 0,
     bed: 0,
-    bathroom: 0
+    bathroom: 0,
   };
   for (let type of lowdown) {
     rooms[type.dataset.typeRoom] = type.value;
@@ -17,6 +17,7 @@ export const calcRoomsWriteInput = (input, lowdown) => {
       : "ванных комнат";
 
   const answer = rooms.bathroom == 0 ? "без" : rooms.bathroom;
-  input.value = `${answer} ${word1}, ${rooms.bed} ${word2}, ${rooms.bathroom} ${word3}`;
+
+  input.value = `${rooms.bedroom} ${word1}, ${rooms.bed} ${word2}, ${answer} ${word3}`;
 };
 // Функция подсчитывает комнаты и записывает в инпут
