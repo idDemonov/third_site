d875348761e9c00dfa57bc647d87c1675dea66cc
demonov <rustamdemonov@gmail.com>
2020-04-06 19:25:52 +0300

    Правка с младенцами

--- src/script/modules/setEventDropBox/calcGuestsWriteInput.ts
+++ src/script/modules/setEventDropBox/calcGuestsWriteInput.ts
@@ -4,14 +4,32 @@ export const calcGuestsWriteInput = (input, lowdown, buttonClear) => {
     children: 0,
     babies: 0,
   };
+
   for (let type of lowdown) {
     people[type.dataset.typeRoom] = type.value;
   }
+  // Считаю количество гостей
   let count = Object.values(people).reduce((a, b) => +a + +b, 0);
-  let word = count == 1 ? "гость" : count < 5 ? "гостя" : "гостей";
+  // Вычитаю младенцев
+  count = count - people.babies;
+
+  // Младенцы не могут поехать одни
+  if (count == 0 && people.babies > 0) {
+    people.adult = 1;
+    lowdown[0].value = 1;
+    count++;
+  }
+  // Формирую слово в инпут, для взрослых
+  let word1 = count == 1 ? "гость" : count < 5 ? "гостя" : "гостей";
+  // Формирую слово в инпут, для младенцев
+  let word2 = people.babies == 1 ? "младенец" : "младенца";
 
   if (count > 0) {
-    input.value = `${count} ${word}`;
+    if (people.babies > 0) {
+      input.value = `${count} ${word1}, ${people.babies} ${word2}`;
+    } else {
+      input.value = `${count} ${word1}`;
+    }
     buttonClear.classList.add("dropdown-selection__clear--activ");
   } else if (count == 0) {
     buttonClear.classList.remove("dropdown-selection__clear--activ");
